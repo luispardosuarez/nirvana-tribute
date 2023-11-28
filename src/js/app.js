46254fe30bc67c94c6f8a4fa3bcc8024498d46ba
Brusmel Gil <Brusmel@outlook.es>
2023-11-28 12:05:20 +0100

    vil

---
+++ src/js/app.js
@@ -0,0 +1,7 @@
+
+document.addEventListener('DOMContentLoaded', function () {
+    var myCarousel = new bootstrap.Carousel(document.getElementById('carousel'), {
+    interval: 5000,
+    wrap: true
+    });
+});
