3fa273f35e8c7355a5410da574ea4b8404ae625a
Brusmel Gil <Brusmel@outlook.es>
2023-12-09 10:59:36 +0100

    modified

--- src/js/app.js
+++ src/js/app.js
@@ -1,14 +1,2 @@
 
 
-const fullImgBox = document.getElementById("fullImgBox");
-fullImg = document.getElementById ("fullImg");
-
-
-function openFullImg(reference){
-    fullImgBox.style.display = "flex";
-    fullImg.src = reference;
-}
-
-function closeImg() {
-    fullImgBox.style.display = "none";
-}
